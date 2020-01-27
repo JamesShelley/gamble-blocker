800709e25a5c39cf729d52ce46e6c62be88a4dc5
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-27 04:14:13 +0000

    block more sites

--- background.js
+++ background.js
@@ -10,8 +10,6 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-console.log("Loaded extension");
-
 function blockRequest(details) {
     return {cancel: true};
 }
@@ -19,7 +17,26 @@ function blockRequest(details) {
 function updateFilters(urls) {
     if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
         chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.bet365.com/*", "*://*.888casino.com/*"]}, ['blocking']);
+    chrome.webRequest.onBeforeRequest.addListener(blockRequest, 
+        {urls: [
+        "*://*.bet365.com/*", 
+        "*://*.888casino.com/*",
+        "*://*.coral.co.uk/*",
+        "*://*.betway.com/*",
+        "*://*.pokerstars.com/*",
+        "*://*.pokerstars.uk/*",
+        "*://*.32red.com/*",
+        "*://*.grosvenorcasinos.com/*",
+        "*://*.paddypower.com/*",
+        "*://*.ladbrokes.com/*",
+        "*://*.virgingames.com/*",
+         "*://*.thesunvegas.co.uk/*",
+         "*://*.skybet.com/*",
+         "*://*.betuk.com/*",
+         "*://*.mansionbet.com/*",
+          "*://*.21.co.uk/*",
+           "*://*.betfair.com/*",
+        ]}, ['blocking']);
 }
 
 updateFilters();
