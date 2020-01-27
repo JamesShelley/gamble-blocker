ab59ff17b007ee4a11db6172460541a56641ffee
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-27 03:54:28 +0000

    block gambling sites from being loaded, need to get a full list

--- background.js
+++ background.js
@@ -1,14 +1,25 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log('The color is green.');
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                pageUrl: {hostEquals: 'bet365.com'},
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
 });
+
+console.log("Loaded extension");
+
+function blockRequest(details) {
+    return {cancel: true};
+}
+
+function updateFilters(urls) {
+    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+        chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.bet365.com/*", "*://*.888casino.com/*"]}, ['blocking']);
+}
+
+updateFilters();
